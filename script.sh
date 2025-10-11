06d2b06fea14724ec6f3daf8a83d86789c9375bb
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-11 16:03:39 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,18 @@
 set -xeuo pipefail
 
 rpm -qa | sort
+
+#curl --create-dirs -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+#dnf install -y tailscale && rm -rf /etc/yum.repos.d/tailscale.repo
+
+#sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+#dnf install -y linux-firmware kernel-modules-extra
+
+# GNOME desktop
+#dnf install -y gdm gnome-shell nautilus ptyxis gnome-disk-utility adw-gtk3-theme
+
+# graphical target
+#systemctl set-default graphical.target
+
+# desktop plumbing/apps
+#dnf install -y flatpak
