19fe5f879f6fb0f4445648c415e5239869252807
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-12 09:53:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,19 +3,23 @@ set -xeuo pipefail
 
 rpm -qa | sort
 
-systemctl mask systemd-remount-fs.service
+rpm -qa 'qemu-user-static*' | xargs dnf remove -y
 
-curl --create-dirs -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf install -y tailscale && rm -rf /etc/yum.repos.d/tailscale.repo
+#systemctl mask systemd-remount-fs.service
+
+#curl --create-dirs -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+#dnf install -y tailscale && rm -rf /etc/yum.repos.d/tailscale.repo
 
 #sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
 #dnf install -y linux-firmware kernel-modules-extra
 
 # GNOME desktop
-dnf install -y gdm gnome-shell nautilus ptyxis gnome-disk-utility adw-gtk3-theme
+#dnf install -y gdm gnome-shell nautilus ptyxis gnome-disk-utility adw-gtk3-theme
 
 # graphical target
-systemctl set-default graphical.target
+#systemctl set-default graphical.target
 
 # desktop plumbing/apps
-dnf install -y flatpak
+#dnf install -y flatpak
+
+rpm -qa | sort
