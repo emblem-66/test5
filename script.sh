0da2819a864f8cdc6fa4b5da84705ef390809830
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-14 21:23:40 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,19 +7,23 @@ rpm -qa 'qemu-user-static*' | xargs dnf remove -y
 
 #systemctl mask systemd-remount-fs.service
 
-#curl --create-dirs -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-#dnf install -y tailscale && rm -rf /etc/yum.repos.d/tailscale.repo
+curl --create-dirs -o /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf install -y tailscale && rm -rf /etc/yum.repos.d/tailscale.repo
+systemctl enable tailscaled.service
+systemctl enable sshd.service
 
-#sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
-#dnf install -y linux-firmware kernel-modules-extra
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+dnf install -y linux-firmware kernel-modules-extra
 
 # GNOME desktop
-#dnf install -y gdm gnome-shell nautilus ptyxis gnome-disk-utility adw-gtk3-theme
+dnf install -y gdm gnome-shell nautilus ptyxis gnome-disk-utility adw-gtk3-theme
 
 # graphical target
-#systemctl set-default graphical.target
+systemctl set-default graphical.target
 
 # desktop plumbing/apps
-#dnf install -y flatpak
+dnf install -y flatpak
+
+systemctl enable fstrim.timer
 
 rpm -qa | sort
